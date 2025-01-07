87cfc4fcd7ad89ad3a6f9bb5d3ce504fd36692a3
Luane Loureiro <luane.loureiro@esx.com.br>
2025-01-07 16:52:23 -0300

    Update Program.cs
    programa refatorado usando switch case

--- vogalOuConsoante/Program.cs
+++ vogalOuConsoante/Program.cs
@@ -1,4 +1,5 @@
-﻿string letra;
+string letra;
+bool repetir = false;
 
 Console.WriteLine("Vou te dizer se vc digitou uma consoante ou uma vogal.");
 
@@ -11,56 +12,73 @@ do
 
     if (letra.Length != 1)
     {
+        repetir = true;
         Console.WriteLine("Entrada inválida! Digite apenas uma letra.");
     }
-    else if (
-                letra == "0" ||
-                letra == "1" ||
-                letra == "2" ||
-                letra == "3" ||
-                letra == "4" ||
-                letra == "5" ||
-                letra == "6" ||
-                letra == "7" ||
-                letra == "8" ||
-                letra == "9"
-            )
-    {
-        Console.WriteLine($"Isso não é uma letra, é um número!");
-    }
-
-    else
-    {
-
-        if (
-            letra == "A" ||
-            letra == "E" ||
-            letra == "I" ||
-            letra == "O" ||
-            letra == "U"
-            )
+    else switch (letra)
         {
-            Console.WriteLine($"A letra {letra} é uma Vogal!");
+            case "0":
+            case "1":
+            case "2":
+            case "3":
+            case "4":
+            case "5":
+            case "6":
+            case "7":
+            case "8":
+            case "9":
+            case "10":
+                Console.WriteLine($"Isso não é uma letra, é um número!");
+                repetir = true;
+                break;
+
+            case "A":
+            case "E":
+            case "I":
+            case "O":
+            case "U":
+                Console.WriteLine($"A letra {letra} é uma Vogal!");
+                Console.WriteLine("Deseja Continuar?");
+                Console.WriteLine("S = Sim e N = Não");
+                string continuar = Console.ReadLine().ToUpper();
+                switch (continuar)
+                {
+                    case "S":
+                        repetir = true;
+                        break;
+                    case "N":
+                        repetir = false;
+                        break;
+                    default:
+                        Console.WriteLine("Opção inválida! Encerrando o programa.");
+                        repetir = false;
+                        break;
+                }
+                break;
+
+            default:
+                Console.WriteLine($"A letra {letra} é uma Consoante!");
+                Console.WriteLine("Deseja Continuar?");
+                Console.WriteLine("S = Sim e N = Não");
+                continuar = Console.ReadLine().ToUpper();
+                switch (continuar)
+                {
+                    case "S":
+                        repetir = true;
+                        break;
+                    case "N":
+                        repetir = false;
+                        break;
+                    default:
+                        Console.WriteLine("Opção inválida! Encerrando o programa.");
+                        repetir = false;
+                        break;
+                }
+                break;
         }
 
-        else
-        {
-            Console.WriteLine($"A letra {letra} é uma Consoante!");
-        }
-
     }
-}
-while (
-    letra.Length != 1 ||
-    letra == "0" ||
-    letra == "1" ||
-    letra == "2" ||
-    letra == "3" ||
-    letra == "4" ||
-    letra == "5" ||
-    letra == "6" ||
-    letra == "7" ||
-    letra == "8" ||
-    letra == "9");
+
+while (repetir == true);
 
 
